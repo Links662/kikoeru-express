70fc0ac0091a8095bd4f105236e3c28981269abb
Links662 <378181557@qq.com>
2026-01-17 20:53:50 +0800

    fix:删除无用文件

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-docker compose down
-docker rmi links/kikoeru
-docker tag links/kikoeru-dev links/kikoeru
-docker rmi links/kikoeru-dev
-
-
--- update.sh
+++
@@ -1,3 +0,0 @@
-docker compose down
-docker rmi links/kikoeru-dev
-docker compose up -d
